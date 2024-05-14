cc78258d1c0d4a3be57e725b9e33e64e52d67a65
Aimee Co <aimeekco@gmail.com>
2024-05-14 16:41:52 -0700

    fix resizing

--- mypage/src/components/ConveyorBelt.js
+++ mypage/src/components/ConveyorBelt.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { Swiper, SwiperSlide } from 'swiper/react';
 import { Navigation, Autoplay, EffectCoverflow, Pagination } from 'swiper/modules';
 
@@ -12,6 +12,27 @@ import sushi from '../images/sushi.png';
 import '../styles/conveyorBelt.css';
 
 const ConveyorBelt = () => {
+  const [slidesPerView, setSlidesPerView] = useState(3);
+
+  useEffect(() => {
+    const handleResize = () => {
+      const width = window.innerWidth;
+      if (width < 768) {
+        setSlidesPerView(1);
+      } else if (width < 1024) {
+        setSlidesPerView(2);
+      } else {
+        setSlidesPerView(3);
+      }
+    };
+
+    handleResize();
+    window.addEventListener('resize', handleResize);
+    return () => {
+      window.removeEventListener('resize', handleResize);
+    };
+  }, []);
+
   const projects = [
     sushi, 
     sushi, 
@@ -28,7 +49,7 @@ const ConveyorBelt = () => {
       navigation={true}
       effect={'coverflow'}
       centeredSlides={true}
-      slidesPerView={'3'}
+      slidesPerView={slidesPerView}
       spaceBetween={30}
       loop={true}
       autoplay={{ 
