56ff404723950d5172218e1f5a55655a4bcd2680
Aimee Co <aimeekco@gmail.com>
2024-05-15 20:36:16 -0700

    mini me

--- mypage/src/components/AboutMe.js
+++ mypage/src/components/AboutMe.js
@@ -1,13 +1,19 @@
 import React from 'react';
+import minime from '../images/minimeturn.png'; 
+import '../styles/aboutMe.css';
 
 function AboutMe() {
+
     return (
-        <div>
-            <div>
-                Fun facts:
+        <div className="aboutme-container">
+            <div className="minime">
+                <img src={minime} alt="Aimee" />
+            </div>
+            <div className="fun-facts">
+                <h2> Hi! I'm Aimee </h2>
             </div>
         </div>
-    )
+    );
 }
 
 export default AboutMe;
