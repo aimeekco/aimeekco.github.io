ab69154265a74439e6f5f3db6f81786aa3c41f7c
Aimee Co <aimeekco@gmail.com>
2024-10-16 23:35:03 -0700

    fix sum stuff

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -5,8 +5,14 @@ import lantern from '../images/lantern.png';
 import hamburger from '../images/hamburger.png';
 
 function Navbar({ setShowLanterns }) {
-  const [menuOpen, setMenuOpen] = useState(false); 
- 
+  const [menuOpen, setMenuOpen] = useState(false);
+  const navItems = {
+    '/': 'About me',
+    '/experience': 'Experience',
+    '/projects': 'Projects',
+    '/contact': 'Contact'
+  };
+
   return (
     <div className="full-container">
       <div className="navbar">
@@ -15,38 +21,25 @@ function Navbar({ setShowLanterns }) {
         </div>
         <div className="title-bar">Aimee's Sushi</div>
       </div>
+
       <div className={`nav-lanterns ${menuOpen ? 'hidden' : ''}`}>
-        <Link to="/">
-          <div className="lanternandtext">
-            <img src={lantern} alt="About me" height={80} className="lantern" />
-            <span> About me </span>
-          </div>
-        </Link>
-        <Link to="/experience">
-          <div className="lanternandtext">
-            <img src={lantern} alt="Experience" height={80} className="lantern" />
-            <span> Experience </span>
-          </div>
-        </Link>
-        <Link to="/projects">
-          <div className="lanternandtext">
-            <img src={lantern} alt="Projects" height={80} className="lantern" />
-            <span> Projects </span>
-          </div>
-        </Link>
-        <Link to="/contact">
-          <div className="lanternandtext">
-            <img src={lantern} alt="Contact" height={80} className="lantern" />
-            <span> Contact </span>
-          </div>
-        </Link>
+        {Object.entries(navItems).map(([path, label]) => (
+          <Link to={path} key={path}>
+            <div className="lanternandtext">
+              <img src={lantern} alt={label} height={80} className="lantern" />
+              <span className="lanternlabel">{label}</span>
+            </div>
+          </Link>
+        ))}
       </div>
+
       {menuOpen && (
         <div className="mobile-menu">
-          <Link to="/" onClick={() => setMenuOpen(false)}>About me</Link>
-          <Link to="/experience" onClick={() => setMenuOpen(false)}>Experience</Link>
-          <Link to="/projects" onClick={() => setMenuOpen(false)}>Projects</Link>
-          <Link to="/contact" onClick={() => setMenuOpen(false)}>Contact</Link>
+          {Object.entries(navItems).map(([path, label]) => (
+            <Link to={path} key={path} onClick={() => setMenuOpen(false)}>
+              {label}
+            </Link>
+          ))}
         </div>
       )}
     </div>
