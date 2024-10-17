812e8c5df191aca46392328a4c42624cf7502ec0
Aimee Co <aimeekco@gmail.com>
2024-10-16 22:22:11 -0700

    add responsiveness

--- src/components/AboutMe.js
+++ src/components/AboutMe.js
@@ -8,6 +8,7 @@ import resume from '../images/Co_Aimee_Resume.pdf';
 
 function AboutMe() {
   const [showLanterns, setShowLanterns] = useState(true);
+  const [isSmallScreen, setIsSmallScreen] = useState(false);
 
   useEffect(() => {
     const handleScroll = () => {
@@ -21,40 +22,58 @@ function AboutMe() {
       }
     };
 
-    window.addEventListener('scroll', handleScroll);
+    const handleResize = () => {
+      const screenWidth = window.innerWidth;
+      setIsSmallScreen(screenWidth <= 640);
+    }
 
-    handleScroll();
+    window.addEventListener('scroll', handleScroll);
+    window.addEventListener('resize', handleResize);
+
+    handleResize();
 
     return () => {
       window.removeEventListener('scroll', handleScroll);
+      window.removeEventListener('resize', handleResize);
     };
   }, []);
 
   return (
-    <div className={`content-container ${showLanterns ? '' : 'reduced-margin'}`}>
-      <div className="aboutme-container">
-        <div className="minime">
-          <img src={minimeturn} alt="Aimee" />
-        </div>
-        <div className="hello">
-          <h1 >Hi! I'm Aimee</h1>
-          <h3>Junior at Harvey Mudd College studying CS + Math</h3>
-          <a href={resume} className="resume-button" target="_blank" rel="noopener noreferrer">
-            View My Resume
-          </a>
-        </div>
-      </div>
-      <div className="panels-container">
-        <Parallax className="panel">
-          <div className="panel-content">
-            <Timeline />
+    <div>
+      {isSmallScreen && (
+        <div id="overlay">
+          <div className="overlay-content">
+            <h1>SCREEN TOO SMALL!</h1>
+            <p>please don't open my website on your phone! (yet)</p>
+            <p>working on it...</p>
           </div>
-        </Parallax>
-        <Parallax className="panel">
-          <div className="panel-content">
-            <PhotoGrid />
+        </div>
+      )}
+      <div className={`content-container ${showLanterns ? '' : 'reduced-margin'}`}>
+        <div className="aboutme-container">
+          <div className="minime">
+            <img src={minimeturn} alt="Aimee" />
           </div>
-        </Parallax>
+          <div className="hello">
+            <h1 >Hi! I'm Aimee</h1>
+            <h3>Junior at Harvey Mudd College studying CS + Math</h3>
+            <a href={resume} className="resume-button" target="_blank" rel="noopener noreferrer">
+              View My Resume
+            </a>
+          </div>
+        </div>
+        <div className="panels-container">
+          <Parallax className="panel">
+            <div className="panel-content">
+              <Timeline />
+            </div>
+          </Parallax>
+          <Parallax className="panel">
+            <div className="panel-content">
+              <PhotoGrid />
+            </div>
+          </Parallax>
+        </div>
       </div>
     </div>
   );
