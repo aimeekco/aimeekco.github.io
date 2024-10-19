ab3bbaf0714189ab28359265c5c6e7b3dc3310bb
Aimee Co <aimeekco@gmail.com>
2024-10-18 20:14:51 -0700

    fix reload

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router } from 'react-router-dom';
+import { HashRouter as Router } from 'react-router-dom';
 import Navbar from './components/Navbar';
 import AppRoutes from './AppRoutes';
 import './App.css';
